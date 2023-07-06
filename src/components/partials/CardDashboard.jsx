9c4826904f3a6206243ee436da99d8be8c494e9c
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 14:41:51 +0800

    Update CardDashboard.jsx

--- src/components/partials/CardDashboard.jsx
+++ src/components/partials/CardDashboard.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
 const CardDashboard = ({ logo, username, followers, subs }) => {
   return (
-    <div>
-      <div>
+    <div className=" bg-light-card-bg dark:bg-dark-card-bg flex flex-col justify-center items-center">
+      <div className="flex justify-center items-center gap-3">
         <img src={logo} alt="" /> {username}
       </div>
       <div>
-        {followers}
-        <p>{subs ? "subscribers" : "followers"}</p>
+        <span className=" text-6xl">{followers}</span>
+        <p className="">{subs ? "subscribers" : "followers"}</p>
       </div>
     </div>
   );
