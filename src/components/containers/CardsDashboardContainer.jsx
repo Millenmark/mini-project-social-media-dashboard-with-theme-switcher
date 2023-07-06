c867faae0d23f08fb2e41d5220801f41cd19ea0f
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 15:14:21 +0800

    Update CardsDashboardContainer.jsx

--- src/components/containers/CardsDashboardContainer.jsx
+++ src/components/containers/CardsDashboardContainer.jsx
@@ -7,17 +7,33 @@ import youtube from "../../assets/icon-youtube.svg";
 const CardsDashboardContainer = () => {
   return (
     <div className="mt-12 flex flex-col gap-6">
-      <CardDashboard logo={facebook} username={"@makkudev"} followers={1233} />
+      <CardDashboard
+        logo={facebook}
+        username={"@makkudev"}
+        followers={1233}
+        className=" bg-facebook"
+      />
 
-      <CardDashboard logo={twitter} username={"@makkudev"} followers={1233} />
+      <CardDashboard
+        logo={twitter}
+        username={"@makkudev"}
+        followers={1233}
+        className="bg-twitter"
+      />
 
-      <CardDashboard logo={instagram} username={"@makkudev"} followers={1233} />
+      <CardDashboard
+        logo={instagram}
+        username={"@makkudev"}
+        followers={1233}
+        className="bg-gradient-to-r from-instagram-gradient-start to-instagram-gradient-end"
+      />
 
       <CardDashboard
         logo={youtube}
         username={"@makkudev"}
         followers={1233}
         subs={true}
+        className="bg-youtube"
       />
     </div>
   );
