52584c24671000559a8187f5d0c277bcc85ae643
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 15:37:44 +0800

    Update CardDashboard.jsx

--- src/components/partials/CardDashboard.jsx
+++ src/components/partials/CardDashboard.jsx
@@ -1,5 +1,17 @@
 /* eslint-disable react/prop-types */
-const CardDashboard = ({ logo, username, followers, subs, className }) => {
+import downArrow from "../../assets/icon-down.svg";
+import upArrow from "../../assets/icon-up.svg";
+import { shortenNumber } from "../../utils/utilityFunctions";
+
+const CardDashboard = ({
+  logo,
+  username,
+  followers,
+  subs,
+  down,
+  up,
+  className,
+}) => {
   return (
     <div className=" bg-light-card-bg dark:bg-dark-card-bg flex flex-col justify-center items-center rounded-lg overflow-hidden">
       <div className={`w-full h-1 ${className}`}></div>
@@ -7,11 +19,18 @@ const CardDashboard = ({ logo, username, followers, subs, className }) => {
         <img src={logo} alt="" /> {username}
       </div>
       <div className="my-5 flex flex-col items-center justify-center">
-        <span className=" text-6xl font-bold mb-1">{followers}</span>
+        <span className=" text-6xl font-bold mb-1">
+          {shortenNumber(followers)}
+        </span>
         <p className=" tracking-[0.45em] uppercase">
           {subs ? "subscribers" : "followers"}
         </p>
       </div>
+      <span>
+        {(up && <img src={upArrow} alt="" />) ||
+          (down && <img src={downArrow} alt="" />)}{" "}
+        {up || down} Today
+      </span>
     </div>
   );
 };
