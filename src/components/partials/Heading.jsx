b77adc481a3132e9b40b02d89c5d3f369f262d7e
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 10:33:56 +0800

    update

---
+++ src/components/partials/Heading.jsx
@@ -0,0 +1,12 @@
+const Heading = () => {
+  return (
+    <div className=" border-b pb-6 dark:border-gray-600 border-gray-700/40">
+      <h1 className="mt-10 text-2xl font-semibold">Social Media Dashboard</h1>
+      <p className="text-sm font-bold capitalize text-light-text dark:text-dark-text">
+        total followers: 24,999
+      </p>
+    </div>
+  );
+};
+
+export default Heading;
