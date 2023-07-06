bb3b9901d658faaf449420dc7ed8be385c676a75
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 19:31:12 +0800

    Update CardOverview.jsx

--- src/components/partials/CardOverview.jsx
+++ src/components/partials/CardOverview.jsx
@@ -1,5 +1,20 @@
-const CardOverview = () => {
-  return <div>CardOverview</div>;
+/* eslint-disable react/prop-types */
+const CardOverview = ({ title, logo, number, status }) => {
+  return (
+    <div>
+      <div>
+        <h3>{title}</h3>
+        <img src={logo} alt="logo" />
+      </div>
+      <div>
+        <span>{number}</span>
+        <span>
+          {status.state}
+          {status.percent}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default CardOverview;
