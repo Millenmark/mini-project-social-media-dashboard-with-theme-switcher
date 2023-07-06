149a8c7a64e42e882d1fe5eb1b92d10f8c7def99
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 14:51:36 +0800

    Update CardDashboard.jsx

--- src/components/partials/CardDashboard.jsx
+++ src/components/partials/CardDashboard.jsx
@@ -2,12 +2,14 @@
 const CardDashboard = ({ logo, username, followers, subs }) => {
   return (
     <div className=" bg-light-card-bg dark:bg-dark-card-bg flex flex-col justify-center items-center">
-      <div className="flex justify-center items-center gap-3">
+      <div className="flex justify-center items-center gap-3 my-5">
         <img src={logo} alt="" /> {username}
       </div>
-      <div>
-        <span className=" text-6xl">{followers}</span>
-        <p className="">{subs ? "subscribers" : "followers"}</p>
+      <div className="my-5 flex flex-col items-center justify-center">
+        <span className=" text-6xl font-bold">{followers}</span>
+        <p className=" tracking-[0.45em] uppercase">
+          {subs ? "subscribers" : "followers"}
+        </p>
       </div>
     </div>
   );
