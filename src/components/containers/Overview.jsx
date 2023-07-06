21f7c87b129c839c260d05b689aa788ea30a72b5
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 19:31:09 +0800

    Update Overview.jsx

--- src/components/containers/Overview.jsx
+++ src/components/containers/Overview.jsx
@@ -1,9 +1,39 @@
+import CardOverview from "../partials/CardOverview";
+import facebook from "../../assets/icon-facebook.svg";
+
+const overviews = [
+  {
+    name: "facebook",
+    title: "page views",
+    number: 87,
+    logo: facebook,
+    status: {
+      state: "up",
+      percent: 2,
+    },
+  },
+];
+
 const Overview = () => {
   return (
     <div className="mt-10">
-      <h2 className="text-2xl font-bold text-light-text dark:text-white-text">
+      <h2 className="text-2xl font-bold text-light-text dark:text-white-text mb-5">
         Overview - Today
       </h2>
+
+      {overviews.map((overview, index) => {
+        const { title, number, logo, status } = overview;
+
+        return (
+          <CardOverview
+            key={index}
+            title={title}
+            number={number}
+            logo={logo}
+            status={status}
+          />
+        );
+      })}
     </div>
   );
 };
