b610cb5ab11b34f5c84781d2d06b41bb6abbbe33
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 19:12:07 +0800

    Create Overview.jsx

---
+++ src/components/containers/Overview.jsx
@@ -0,0 +1,11 @@
+const Overview = () => {
+  return (
+    <div className="mt-10">
+      <h2 className="text-2xl font-bold text-light-text dark:text-white-text">
+        Overview - Today
+      </h2>
+    </div>
+  );
+};
+
+export default Overview;
