cc59356c6192e1da8e83c5fc7fe32e325a4bb080
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 11:01:47 +0800

    Create Toggle.jsx

---
+++ src/components/partials/Toggle.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { ToggleContext } from "../../context/ToggleContext";
+
+const Toggle = () => {
+  const { isToggleActive, handleToggleActive } = useContext(ToggleContext);
+
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center cursor-pointer z-30">
+      <div className="flex justify-center items-center">
+        <div
+          className={`w-12 h-7 flex items-center bg-gradient-to-r from-primary-gradient-start to-primary-gradient-end rounded-full mx-3 px-1 ${
+            isToggleActive ? "bg-cyan-700" : ""
+          }`}
+          onClick={handleToggleActive}
+        >
+          {/* Switch */}
+          <div
+            className={`bg-white w-5 h-5 transition-all duration-300 rounded-full shadow-md transform ${
+              isToggleActive ? "translate-x-5" : ""
+            }`}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Toggle;
