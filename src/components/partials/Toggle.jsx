5a3014bd2cb69c886de352c4f9660b3643dd975f
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 11:17:13 +0800

    Update Toggle.jsx

--- src/components/partials/Toggle.jsx
+++ src/components/partials/Toggle.jsx
@@ -5,18 +5,21 @@ const Toggle = () => {
   const { isToggleActive, handleToggleActive } = useContext(ToggleContext);
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center cursor-pointer z-30">
-      <div className="flex justify-center items-center">
+    <div className="w-full h-full flex flex-col justify-center items-center cursor-pointer z-30 pt-4">
+      <div className="flex justify-between w-full items-center">
+        <span className=" text-light-text dark:text-dark-text text-sm font-bold">
+          Dark Mode
+        </span>
         <div
-          className={`w-12 h-7 flex items-center bg-gradient-to-r from-primary-gradient-start to-primary-gradient-end rounded-full mx-3 px-1 ${
-            isToggleActive ? "bg-cyan-700" : ""
+          className={` w-14 h-7 flex items-center bg-gradient-to-r from-primary-gradient-start to-primary-gradient-end rounded-full px-1 ${
+            isToggleActive ? "bg-cyan-700" : " bg-light-toggle"
           }`}
           onClick={handleToggleActive}
         >
           {/* Switch */}
           <div
             className={`bg-white w-5 h-5 transition-all duration-300 rounded-full shadow-md transform ${
-              isToggleActive ? "translate-x-5" : ""
+              isToggleActive ? "translate-x-7" : ""
             }`}
           ></div>
         </div>
