5232d38fedc19b3b26cca9ea07c2deb7eb495dd4
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-06 14:26:06 +0800

    Update CardDashboard.jsx

--- src/components/partials/CardDashboard.jsx
+++ src/components/partials/CardDashboard.jsx
@@ -1,5 +1,16 @@
-const CardDashboard = () => {
-  return <div>CardDashboard</div>;
+/* eslint-disable react/prop-types */
+const CardDashboard = ({ logo, username, followers, subs }) => {
+  return (
+    <div>
+      <div>
+        <img src={logo} alt="" /> {username}
+      </div>
+      <div>
+        {followers}
+        <p>{subs ? "subscribers" : "followers"}</p>
+      </div>
+    </div>
+  );
 };
 
 export default CardDashboard;
