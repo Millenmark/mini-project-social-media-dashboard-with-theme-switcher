a7d6f388087cf75d52867d8652e58d21e398a66f
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-07 17:22:14 +0800

    Update Heading.jsx

--- src/components/partials/Heading.jsx
+++ src/components/partials/Heading.jsx
@@ -1,6 +1,6 @@
 const Heading = () => {
   return (
-    <div className=" border-b pb-6 dark:border-gray-600 border-gray-700/40">
+    <div className=" border-b md:border-none pb-6 dark:border-gray-600 border-gray-700/40">
       <h1 className="mt-10 text-2xl font-semibold">Social Media Dashboard</h1>
       <p className="text-sm font-bold capitalize text-light-text dark:text-dark-text">
         total followers: 24,999
