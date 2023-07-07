a734746449e98d8e459a2156144efbb7efa14c19
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-07 16:43:49 +0800

    Update Dashboard.jsx

--- src/components/containers/Dashboard.jsx
+++ src/components/containers/Dashboard.jsx
@@ -3,7 +3,7 @@ import { socials } from "../../services/contents";
 
 const Dashboard = () => {
   return (
-    <div className="mt-12 flex flex-col gap-6">
+    <div className="mt-12 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
       {socials.map((social, index) => {
         const { logo, username, followers, colorScheme, state } = social;
 
