f392f379763272cb635e16992ad56d5bbd4fbc33
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-07 12:54:19 +0800

    Create contents.js

---
+++ src/services/contents.js
@@ -0,0 +1,14 @@
+import facebook from "../assets/icon-facebook.svg";
+
+export const overviews = [
+  {
+    name: "facebook",
+    title: "page views",
+    number: 87,
+    logo: facebook,
+    status: {
+      state: "up",
+      percent: 2,
+    },
+  },
+];
