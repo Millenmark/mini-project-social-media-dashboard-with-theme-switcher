b1c2cb77b5738228d5b4c2cfbce67a35ddf2ae15
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-07 13:41:46 +0800

    Update Dashboard.jsx

--- src/components/containers/Dashboard.jsx
+++ src/components/containers/Dashboard.jsx
@@ -1,44 +1,23 @@
 import CardDashboard from "../partials/CardDashboard";
-import facebook from "../../assets/icon-facebook.svg";
-import twitter from "../../assets/icon-twitter.svg";
-import instagram from "../../assets/icon-instagram.svg";
-import youtube from "../../assets/icon-youtube.svg";
+import { socials } from "../../services/contents";
 
 const Dashboard = () => {
   return (
     <div className="mt-12 flex flex-col gap-6">
-      <CardDashboard
-        logo={facebook}
-        username={"@makkudev"}
-        followers={1987}
-        className=" bg-facebook"
-        up={12}
-      />
+      {socials.map((social, index) => {
+        const { logo, username, followers, colorScheme, state } = social;
 
-      <CardDashboard
-        logo={twitter}
-        username={"@makkudev"}
-        followers={1044}
-        className="bg-twitter"
-        up={99}
-      />
-
-      <CardDashboard
-        logo={instagram}
-        username={"@makkudev"}
-        followers={11234}
-        className="bg-gradient-to-r from-instagram-gradient-start to-instagram-gradient-end"
-        up={1099}
-      />
-
-      <CardDashboard
-        logo={youtube}
-        username={"Makku Dev"}
-        followers={1233}
-        subs={true}
-        className="bg-youtube"
-        down={144}
-      />
+        return (
+          <CardDashboard
+            key={index}
+            logo={logo}
+            username={username}
+            followers={followers}
+            className={colorScheme}
+            state={state}
+          />
+        );
+      })}
     </div>
   );
 };
