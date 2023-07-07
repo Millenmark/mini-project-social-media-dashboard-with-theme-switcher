9e442599f1b0cdcf481a35b6823231210a54b7c9
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-07 13:19:10 +0800

    Update contents.js

--- src/services/contents.js
+++ src/services/contents.js
@@ -1,5 +1,7 @@
 import facebook from "../assets/icon-facebook.svg";
 import instagram from "../assets/icon-instagram.svg";
+import twitter from "../assets/icon-twitter.svg";
+import youtube from "../assets/icon-youtube.svg";
 
 export const overviews = [
   {
@@ -39,7 +41,47 @@ export const overviews = [
     logo: instagram,
     status: {
       state: "up",
-      percent: 2257,
+      percent: 1375,
+    },
+  },
+  {
+    name: "twitter",
+    title: "retweets",
+    number: 117,
+    logo: twitter,
+    status: {
+      state: "up",
+      percent: 303,
+    },
+  },
+  {
+    name: "twitter",
+    title: "likes",
+    number: 507,
+    logo: twitter,
+    status: {
+      state: "up",
+      percent: 553,
+    },
+  },
+  {
+    name: "youtube",
+    title: "likes",
+    number: 107,
+    logo: youtube,
+    status: {
+      state: "down",
+      percent: 19,
+    },
+  },
+  {
+    name: "youtube",
+    title: "total views",
+    number: 1407,
+    logo: youtube,
+    status: {
+      state: "down",
+      percent: 12,
     },
   },
 ];
